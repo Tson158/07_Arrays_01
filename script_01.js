e90e4c46d80e9b2fe6ebbb6789106d0a945342d0
Kakarott <Sonntag_thomas_86@web.de>
2021-11-24 09:32:05 +0100

    Funktion: Satz ++

--- script_01.js
+++ script_01.js
@@ -1,6 +1,22 @@
 /* Die Satzbau-Maschine | Arrays */
 
 
+/**** 01. Funktionalität mit Einzelparameter ****/
+// ---> "Ich bin Thomas Sonntag"
+
+output(getSentence("Ich","bin","Thomas","Sonntag"))
+function getSentence(wort1,wort2,wort3,wort4) {
+
+    const gap = " ";
+    const pkt = ".";
+    const str = wort1 + gap + 
+                wort2 + gap + 
+                wort3 + gap + 
+                wort4 +
+                pkt;
+
+    return str  //"Ich bin Thomas Sonntag"
+}
 
 
 
